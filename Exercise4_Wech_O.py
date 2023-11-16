1476452b26dc07d6fc303a4a818815c75622f998
WECH-yee <oramootw@gmail.com>
2023-11-16 14:03:50 +0700

    Exercise 4 Your Score
    สร้างตัวแปรเพื่อเก็บคะแนนวิชานั้น และนำมาแสดงผล

---
+++ Exercise4_Wech_O.py
@@ -0,0 +1,9 @@
+Found_Eng = 85
+Gene_Bus = 80
+Intro_Com_Sys = 81
+Com_Pro = 85
+print("---Your Score---")
+print("Foundation English               : ", Found_Eng)
+print("General Business                 : ", Gene_Bus)
+print("Introduction to Computer Systems : ", Intro_Com_Sys)
+print("Computer Programming             : ", Com_Pro)
